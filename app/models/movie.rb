afc844c44d0b5fcb5935833794f2360e2b6369da
James Burns <jrburns96@aol.com>
2021-12-17 15:35:18 -0500

    commit tree 888b0ebe687e6d4b1aa9a34c4d20dbd91fe54e28

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,47 @@
 class Movie < ActiveRecord::Base
+   
+    def self.create_with_title(title)
+        Movie.create(title:"#{title}")
+    end
 
+    def self.first_movie
+        Movie.first
+    end
+    def self.last_movie
+        Movie.last
+    end
+    
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find_by id: id
+    end
+
+    def self.find_movie_with_attributes(attr1, attr2=nil)
+        Movie.find_by attr1
+        # Movie.where("title = ?",)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+        # console log what movie has & query that?
+    end
+
+    def update_with_attributes(attr1)
+        self.update(attr1)
+    end
+
+    def self.update_all_titles(thing)
+        Movie.update(title: thing)
+    end
+
+    def self.delete_by_id(thing)
+        # Movie.find(thing).destroy
+        Movie.destroy(thing)
+    end
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 end
